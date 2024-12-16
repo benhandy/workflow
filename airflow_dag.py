78cda9e343e2a2537c2b24e2bba8b6184f4fb282
Ben Handy <benhandy9@gmail.com>
2024-12-16 15:47:23 -0500

    Update airflow_dag.py

--- airflow_dag.py
+++ airflow_dag.py
@@ -563,7 +563,196 @@ with DAG(
         op_kwargs={"tables": tables}, 
     )
 
+
+
+    
     end_task = DummyOperator(task_id="end")
 
     start_task >> task_group >> notification_task >> end_task
 
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+## Lastly, I collaborated with the Machine Learning team to build a pipeline 
+## for three fictitious Mobility-as-a-Service vendors: 
+## Alitran, Easy Destiny, and ToMyPlaceAI. 
+## The goal was to preprocess and validate data for training a model 
+## to estimate ride durations. 
+
+## The pipeline included evaluating training metrics to determine 
+## whether the model was suitable for deployment. 
+## By enabling continuous training and evaluation, the solution 
+## helped each vendor improve their ride duration estimation 
+## services over time.
+
+## Here is the code from my bash terminal in Cloud-9 
+
+voclabs:~/environment $ aws s3 cp --recursive s3://dlai-data-engineering/labs/c2w4a1-756683/ ./
+download: s3://dlai-data-engineering/labs/c2w4a1-756683/C2_W4_Assignment.md to ./C2_W4_Assignment.mddownload: s3://dlai-data-engineering/labs/c2w4a1-756683/data/work_zone/data_science_project/datasets/alitran/test.parquet to data/work_zone/data_science_project/datasets/alitran/test.parquetdownload: s3://dlai-data-engineering/labs/c2w4a1-756683/data/work_zone/data_science_project/datasets/alitran/train.parquet to data/work_zone/data_science_project/datasets/alitran/train.parquetdownload: s3://dlai-data-engineering/labs/c2w4a1-756683/data/work_zone/data_science_project/datasets/to_my_place_ai/train.parquet to data/work_zone/data_science_project/datasets/to_my_place_ai/train.parquetdownload: s3://dlai-data-engineering/labs/c2w4a1-756683/data/work_zone/data_science_project/datasets/easy_destiny/test.parquet to data/work_zone/data_science_project/datasets/easy_destiny/test.parquetdownload: s3://dlai-data-engineering/labs/c2w4a1-756683/src/model_trip_duration_easy_destiny.py to src/model_trip_duration_easy_destiny.py
+download: s3://dlai-data-engineering/labs/c2w4a1-756683/images/DAG_outline.png to images/DAG_outline.pngdownload: s3://dlai-data-engineering/labs/c2w4a1-756683/scripts/restart_airflow.sh to scripts/restart_airflow.shdownload: s3://dlai-data-engineering/labs/c2w4a1-756683/src/templates/generate_dags.py to src/templates/generate_dags.pydownload: s3://dlai-data-engineering/labs/c2w4a1-756683/data/work_zone/data_science_project/train.parquet to data/work_zone/data_science_project/train.parquetdownload: s3://dlai-data-engineering/labs/c2w4a1-756683/data/work_zone/data_science_project/test.parquet to data/work_zone/data_science_project/test.parquetdownload: s3://dlai-data-engineering/labs/c2w4a1-756683/data/work_zone/data_science_project/datasets/easy_destiny/train.parquet to data/work_zone/data_science_project/datasets/easy_destiny/train.parquetdownload: s3://dlai-data-engineering/labs/c2w4a1-756683/data/work_zone/data_science_project/datasets/to_my_place_ai/test.parquet to data/work_zone/data_science_project/datasets/to_my_place_ai/test.parquet
+voclabs:~/environment $ 
+voclabs:~/environment $ cd data
+voclabs:~/environment $ cd data
+voclabs:~/environment/data $ aws s3 sync work_zone s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/
+
+upload: work_zone/data_science_project/datasets/alitran/test.parquet to s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/data_science_project/datasets/alitran/test.parquet
+upload: work_zone/data_science_project/datasets/alitran/train.parquet to s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/data_science_project/datasets/alitran/train.parquet
+upload: work_zone/data_science_project/test.parquet to s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/data_science_project/test.parquet
+upload: work_zone/data_science_project/train.parquet to s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/data_science_project/train.parquet
+upload: work_zone/data_science_project/datasets/easy_destiny/test.parquet to s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/data_science_project/datasets/easy_destiny/test.parquet
+upload: work_zone/data_science_project/datasets/to_my_place_ai/test.parquet to s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/data_science_project/datasets/to_my_place_ai/test.parquet
+upload: work_zone/data_science_project/datasets/to_my_place_ai/train.parquet to s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/data_science_project/datasets/to_my_place_ai/train.parquet
+upload: work_zone/data_science_project/datasets/easy_destiny/train.parquet to s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/data_science_project/datasets/easy_destiny/train.parquet
+voclabs:~/environment/data $ 
+voclabs:~/environment/data $ cd ..
+voclabs:~/environment $ 
+voclabs:~/environment $ aws s3 ls s3://de-c2w4a1-992382772725-us-east-1-raw-data/work_zone/ --recursive
+
+2024-12-16 20:04:54     508420 work_zone/data_science_project/datasets/alitran/test.parquet
+2024-12-16 20:04:54     520838 work_zone/data_science_project/datasets/alitran/train.parquet
+2024-12-16 20:04:54     509307 work_zone/data_science_project/datasets/easy_destiny/test.parquet
+2024-12-16 20:04:54     522152 work_zone/data_science_project/datasets/easy_destiny/train.parquet
+2024-12-16 20:04:54     508515 work_zone/data_science_project/datasets/to_my_place_ai/test.parquet
+2024-12-16 20:04:54     517511 work_zone/data_science_project/datasets/to_my_place_ai/train.parquet
+2024-12-16 20:04:54     517201 work_zone/data_science_project/test.parquet
+2024-12-16 20:04:54     548293 work_zone/data_science_project/train.parquet
+voclabs:~/environment $ 
+voclabs:~/environment $ cp src/model_trip_duration_easy_destiny.py src/templates/template.py
+voclabs:~/environment $ 
+voclabs:~/environment $ mkdir -p src/templates/dag_configs
+voclabs:~/environment $ 
+voclabs:~/environment $ cd src/templates
+voclabs:~/environment/src/templates $ 
+voclabs:~/environment/src/templates $ python3 ./generate_dags.py
+folder_configs dag_configs
+Created ./../dags/model_trip_duration_easy_destiny.py from config: config_easy_destiny.json...
+Created ./../dags/model_trip_duration_alitran.py from config: config_alitran.json...
+Created ./../dags/model_trip_duration_to_my_place_ai.py from config: config_to_my_place_ai.json...
+voclabs:~/environment/src/templates $ 
+voclabs:~/environment/src/templates $ cd ../..
+voclabs:~/environment $ 
+voclabs:~/environment $ aws s3 sync src/dags s3://de-c2w4a1-992382772725-us-east-1-mwaa-dags/dags
+
+upload: src/dags/model_trip_duration_alitran.py to s3://de-c2w4a1-992382772725-us-east-1-mwaa-dags/dags/model_trip_duration_alitran.py
+upload: src/dags/model_trip_duration_to_my_place_ai.py to s3://de-c2w4a1-992382772725-us-east-1-mwaa-dags/dags/model_trip_duration_to_my_place_ai.py
+upload: src/dags/model_trip_duration_easy_destiny.py to s3://de-c2w4a1-992382772725-us-east-1-mwaa-dags/dags/model_trip_duration_easy_destiny.py
+voclabs:~/environment $ 
+voclabs:~/environment $ 
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+"""
+                    Building a Machine Learning Pipeline
+- Supports Mobility-as-a-Service vendors (Alitran, Easy Destiny, ToMyPlaceAI).
+- Pipeline preprocesses and validates data, trains a model for ride duration prediction, and evaluates deployment suitability.
+- Implements:
+  - TaskFlow API for DAG creation
+  - Great Expectations for data quality checks
+  - BranchPythonOperator for conditional workflow paths
+  - Dynamic DAGs using Jinja templates and config files
+"""
+
+import os
+from datetime import datetime
+import pandas as pd
+import numpy as np
+from scipy.stats import linregress
+from airflow.decorators import dag, task
+from airflow.models import Variable
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import BranchPythonOperator
+from great_expectations_provider.operators.great_expectations import GreatExpectationsOperator
+
+@dag(
+    schedule_interval="@daily",
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    default_args={"retries": 2},
+    tags=["dynamic_dag__model_train"],
+)
+def model_trip_duration_easy_destiny():
+    vendor_name = "easy_destiny"
+    start_task = DummyOperator(task_id="start")
+
+    data_quality_task = GreatExpectationsOperator(
+        task_id="data_quality",
+        data_context_root_dir="./dags/gx",
+        data_asset_name="train_easy_destiny",
+        dataframe_to_validate=pd.read_parquet(
+            f"s3://{Variable.get('bucket_name')}/work_zone/data_science_project/datasets/{vendor_name}/train.parquet"
+        ),
+        execution_engine="PandasExecutionEngine",
+        expectation_suite_name="de-c2w4a1-expectation-suite",
+        return_json_dict=True,
+        fail_task_on_validation_failure=True,
+    )
+
+    @task
+    def train_and_evaluate(bucket_name: str, vendor_name: str):
+        datasets_path = f"s3://{bucket_name}/work_zone/data_science_project/datasets"
+        train = pd.read_parquet(f"{datasets_path}/{vendor_name}/train.parquet")
+        test = pd.read_parquet(f"{datasets_path}/{vendor_name}/test.parquet")
+
+        X_train = train[["distance"]].to_numpy()[:, 0]
+        X_test = test[["distance"]].to_numpy()[:, 0]
+        y_train = train[["trip_duration"]].to_numpy()[:, 0]
+        y_test = test[["trip_duration"]].to_numpy()[:, 0]
+
+        model = linregress(X_train, y_train)
+        y_pred_test = model.slope * X_test + model.intercept
+        performance = np.sqrt(np.average((y_pred_test - y_test) ** 2))
+        print("--- performance RMSE ---")
+        print(f"test: {performance:.2f}")
+
+        return performance
+
+    def _is_deployable(ti):
+        performance = ti.xcom_pull(task_ids="train_and_evaluate")
+        if performance < 500:
+            print(f"is deployable: {performance}")
+            return "deploy"
+        else:
+            print("is not deployable")
+            return "notify"
+
+    is_deployable_task = BranchPythonOperator(
+        task_id="is_deployable",
+        python_callable=_is_deployable,
+        do_xcom_push=False,
+    )
+
+    @task
+    def deploy():
+        print("Deploying...")
+
+    @task
+    def notify(message):
+        print(f"{message}. Notify to mail: admin@easy_destiny.com")
+
+    end_task = DummyOperator(task_id="end", trigger_rule="none_failed_or_skipped")
+
+    (
+        start_task
+        >> data_quality_task
+        >> train_and_evaluate(
+            bucket_name="{{ var.value.bucket_name }}",
+            vendor_name="easy_destiny",
+        )
+        >> is_deployable_task
+        >> [deploy(), notify("Not deployed")]
+        >> end_task
+    )
+
+dag_model_trip_duration_easy_destiny = model_trip_duration_easy_destiny()
