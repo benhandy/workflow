3502e91d506d3723293ba41a314d499f58d5c7f8
Ben Handy <benhandy9@gmail.com>
2024-12-16 11:06:54 -0500

    Update airflow_dag.py

--- airflow_dag.py
+++ airflow_dag.py
@@ -466,4 +466,103 @@ with DAG(
 #                    You will also use a PythonOperator which will call the drop_nas_and_duplicates function.
 
 
+import datetime as dt
+import re
+import pandas as pd
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.amazon.aws.transfers.sql_to_s3 import SqlToS3Operator
+from airflow.utils.context import Context
+from airflow.utils.task_group import TaskGroup
+
+S3_URI_PATTERN = r"^s3://[a-zA-Z0-9.\-_]+(/[a-zA-Z0-9.\-_]+)*$"
+tables = ["payments", "customers", "products"]
+
+def drop_nas_and_duplicates(
+    source_bucket: str,
+    source_key: str,
+    target_key: str,
+    target_bucket: str = "",
+    **context: Context,
+):
+    if not target_bucket:
+        target_bucket = source_bucket
+
+    source_s3_uri = f"s3://{source_bucket}/{source_key}"
+    target_s3_uri = f"s3://{target_bucket}/{target_key}"
+
+    assert re.match(S3_URI_PATTERN, source_s3_uri)
+    assert re.match(S3_URI_PATTERN, target_s3_uri)
+
+    df = pd.read_csv(source_s3_uri)
+
+    df = df.dropna()
+    df = df.drop_duplicates()
+
+    df.to_csv(target_s3_uri)
+
+    num_valid_records = len(df)
+    context["ti"].xcom_push(key="valid_records", value=num_valid_records)
+
+def notify_valid_records(tables: list[str], **context: Context):
+    for table in tables:
+        task_id = f"transform_{table}"
+        task_id = f"{table}.{task_id}" if len(tables) > 1 else task_id
+        
+        valid_records = context["ti"].xcom_pull(
+            task_ids=[task_id], key="valid_records"
+        )
+
+        print(f"Number of valid records in table {table}: {valid_records}")
+
+
+with DAG(
+    dag_id="grouped_tasks_dag",
+    schedule="@daily",
+    start_date=dt.datetime(year=2024, month=4, day=1),
+    catchup=False,
+) as dag:
+    partition_date = '{{ macros.ds_format(ds, "%Y-%m-%d", "%Y/%m/%d") }}'
+
+    start_task = DummyOperator(task_id="start")
+
+    task_group = []
+    for table in tables:
+        with TaskGroup(table) as etl_tg:
+            
+            extract_load_task = SqlToS3Operator(
+                task_id=f"extract_load_{table}",
+                sql_conn_id="mysql_connection",
+                query=f"SELECT * FROM {table};",
+                s3_bucket=Variable.get("s3_bucket"),
+                s3_key=f"bronze/{partition_date}/{table}.csv",
+                replace=True,
+            )
+
+            transform_task = PythonOperator(
+                task_id=f"transform_{table}",
+                python_callable=drop_nas_and_duplicates,
+                provide_context=True,
+                op_kwargs={
+                    "source_bucket": Variable.get("s3_bucket"),
+                    "source_key": f"bronze/{partition_date}/{table}.csv",
+                    "target_key": f"silver/{partition_date}/{table}.csv",
+                },
+            )
+            
+            extract_load_task >> transform_task
+            task_group.append(etl_tg)
+
+    notification_task = PythonOperator(
+        task_id="notification",
+        python_callable=notify_valid_records, 
+        provide_context=True,
+        op_kwargs={"tables": tables}, 
+    )
+
+    end_task = DummyOperator(task_id="end")
+
+    start_task >> task_group >> notification_task >> end_task
 
