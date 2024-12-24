2acb312a5a9af9c462ad55b8f76104da2eb263f6
Ben Handy <benhandy9@gmail.com>
2024-12-24 12:34:59 -0500

    Update query optimization.py

--- query optimization.py
+++ query optimization.py
@@ -73,4 +73,130 @@ REDSHIFTDBNAME = os.getenv('REDSHIFTDBNAME')
 REDSHIFTDBUSER = os.getenv('REDSHIFTDBUSER')
 REDSHIFTDBPASSWORD = os.getenv('REDSHIFTDBPASSWORD')
 
+# creating connection string
+
+redshift_connection_url = f'redshift+psycopg2://{REDSHIFTDBUSER}:{REDSHIFTDBPASSWORD}@{REDSHIFTDBHOST}:{REDSHIFTDBPORT}/{REDSHIFTDBNAME}'
+print(redshift_connection_url)
+
+"""
+Printed connection string: 
+
+redshift+psycopg2://defaultuser:Defaultuserpwrd1234+@redshiftcluster-9bnjth01mfim.coo7fo01pnn3.us-east-1.redshift.amazonaws.com:5439/dev
+
+"""
+# connecting to redshift cluster 
+%sql {redshift_connection_url}
+
+# disable caching results
+%sql SET enable_result_cache_for_session TO off;
+
+
+    ///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////
+
+    
+%%sql
+SELECT  distinct tablename
+FROM PG_TABLE_DEF
+WHERE schemaname='public'
+;
+
+
+%%sql
+SELECT  *
+FROM PG_TABLE_DEF
+WHERE schemaname='public'
+AND tablename='lineitem'
+;
+
+
+"""
+
+In the CTE: I need to add the order keys that I want to inspect into the IN operator. Specifically, I'll include the order keys 1552449, 13620130, and 45619461.
+In the SELECT statement: I will select the necessary columns from each table. Specifically:
+l_orderkey and l_partkey from the lineitemorders CTE.
+p_name from the part table.
+c_custkey from the customer table.
+o_orderstatus and o_orderdate from the orders table.
+n_name from the nation table.
+r_name from the region table.
+Join Statements: I will perform 5 joins:
+I will join the part table on the p_partkey column and the l_partkey column from the lineitemorders CTE.
+Then, I'll join the orders table on the l_orderkey column and the o_orderkey column from the orders table.
+Next, I'll join the customer table on the c_custkey column and the o_custkey column from the orders table.
+I'll then join the nation table on the n_nationkey column and the c_nationkey column from the customer table.
+Finally, I'll join the region table on the r_regionkey column and the n_regionkey column from the nation table.
+
+"""
+
+
+%%timeit -n1 -r1
+raw_sql_statement =     
+    WITH lineitemorders AS (
+        SELECT *
+        FROM public.lineitem
+        WHERE l_orderkey IN (1552449, 13620130, 45619461)
+    )
+    
+    SELECT DISTINCT 
+        lio.l_orderkey, 
+        lio.l_partkey, 
+        pt.p_name, 
+        ctr.c_custkey, 
+        ord.o_orderstatus, 
+        ord.o_orderdate, 
+        ntn.n_name, 
+        rgn.r_name
+    FROM lineitemorders lio
+    JOIN public.part pt ON pt.p_partkey = lio.l_partkey
+    JOIN public.orders ord ON lio.l_orderkey = ord.o_orderkey
+    JOIN public.customer ctr ON ctr.c_custkey = ord.o_custkey
+    JOIN public.nation ntn ON ntn.n_nationkey = ctr.c_nationkey
+    JOIN public.region rgn ON rgn.r_regionkey = ntn.n_regionkey;
+
+
+sql_statement = format_query(query=raw_sql_statement)
+
+%sql {sql_statement}
+
+
+
+"""
+
+Create a CTE (Common Table Expression) called avg_balance_middle_east. In this CTE, I will:
+Compute the average account balance (AVG()) of customers (c_acctbal) from the customer table.
+Join the nation table on n_nationkey and c_nationkey to connect the customer data with the nation information.
+Join the region table on r_regionkey and n_regionkey to associate the nation data with the region.
+Filter by the region name 'MIDDLE EAST' in the region table.
+In the main query:
+I will select the count of distinct customers (COUNT(DISTINCT c.c_custkey)) whose account balance is greater than the average balance computed in the CTE.
+I need to join the nation and region tables again on the same columns (n_nationkey and c_nationkey for nation; r_regionkey and n_regionkey for region).
+I will apply two filters:
+The region should be 'MIDDLE EAST'.
+The customer’s account balance (c_acctbal) should be greater than the avg_balance computed in the CTE. 
+I’ll need to use a subquery to extract the avg_balance from the CTE and apply it in the filter.
+
+
+"""
+
+
+
+%%timeit -n1 -r1
+WITH avg_balance_middle_east AS (
+    SELECT 
+        AVG(c_acctbal) AS avg_balance
+    FROM customer c
+    JOIN nation n ON c.c_nationkey = n.n_nationkey
+    JOIN region r ON n.n_regionkey = r.r_regionkey
+    WHERE r.r_name = 'MIDDLE EAST'
+)
+SELECT 
+    COUNT(DISTINCT c.c_custkey) AS num_customers_above_avg_balance
+FROM customer c
+JOIN nation n ON c.c_nationkey = n.n_nationkey
+JOIN region r ON n.n_regionkey = r.r_regionkey
+WHERE r.r_name = 'MIDDLE EAST'
+  AND c.c_acctbal > (SELECT avg_balance FROM avg_balance_middle_east);
+
+
 
