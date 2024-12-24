0203f207d9174e6a5854a492c186f6b0633dd548
Ben Handy <benhandy9@gmail.com>
2024-12-24 12:23:38 -0500

    Update query optimization.py

--- query optimization.py
+++ query optimization.py
@@ -1,5 +1,11 @@
-## I use a Amazon Redshift database that leverages columnar storage, and an Amazon RDS (Relational Database Service) PostgreSQL data that leverages row-oriented storage
-## To assess the execution time of the analytical queries, I work with a benchmarking dataset and run 5 analytical queries to query the data from each store 
+"""
+I use a Amazon Redshift database that leverages columnar storage, 
+and an Amazon RDS (Relational Database Service) PostgreSQL data that leverages row-oriented storage.
+
+To assess the execution time of the analytical queries, I work with a benchmarking dataset 
+and run 5 analytical queries to query the data from each store.
+
+"""
 
 
 import os 
@@ -41,4 +47,30 @@ def format_query(query: str='', path: str =None) -> str:
     sql_command = f"/* Query ID {query_uuid} */{query};"    
     
     return sql_command
+
+""" 
+    Here is the env file which includes endpoints and connection 
+    information for both the Redshift and Postgresql databases
+
+RDSDBHOST=de-c3w3lab1-rds.cz6m82e4wt2x.us-east-1.rds.amazonaws.com
+RDSDBPORT=5432
+RDSDBNAME=dev
+RDSDBUSER=postgresuser
+RDSDBPASSWORD=adminpwrd
+REDSHIFTDBHOST=redshiftcluster-9bnjth01mfim.coo7fo01pnn3.us-east-1.redshift.amazonaws.com
+REDSHIFTDBPORT=5439
+REDSHIFTDBNAME=dev
+REDSHIFTDBUSER=defaultuser
+REDSHIFTDBPASSWORD=Defaultuserpwrd1234+
+
+"""
+
+load_dotenv('./src/env', override=True)
+
+REDSHIFTDBHOST = os.getenv('REDSHIFTDBHOST')
+REDSHIFTDBPORT = int(os.getenv('REDSHIFTDBPORT'))
+REDSHIFTDBNAME = os.getenv('REDSHIFTDBNAME')
+REDSHIFTDBUSER = os.getenv('REDSHIFTDBUSER')
+REDSHIFTDBPASSWORD = os.getenv('REDSHIFTDBPASSWORD')
+
 
