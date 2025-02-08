29bef237f5cf9fd1210b55fc24c87460a94106fc
Ben Handy <benhandy9@gmail.com>
2025-02-08 17:50:34 -0500

    Update query optimization.py

--- query optimization.py
+++ query optimization.py
@@ -5,7 +5,7 @@ I use a Amazon Redshift database that leverages columnar storage,
 and an Amazon RDS (Relational Database Service) PostgreSQL data that leverages row-oriented storage
 
 To assess the execution time of the analytical queries, I work with a benchmarking dataset 
-and run 5 analytical queries to query the data from each store.
+and run 5 analytical queries to query the data from each store
 
 """
 
