a30b52abc9e805762e77dd2366ef7638bec8d2a9
Ben Handy <benhandy9@gmail.com>
2025-02-08 17:50:26 -0500

    Update query optimization.py

--- query optimization.py
+++ query optimization.py
@@ -2,7 +2,7 @@
 This is a SQL query optimization lab that I completed in a data engineering course with AWS
 
 I use a Amazon Redshift database that leverages columnar storage, 
-and an Amazon RDS (Relational Database Service) PostgreSQL data that leverages row-oriented storage.
+and an Amazon RDS (Relational Database Service) PostgreSQL data that leverages row-oriented storage
 
 To assess the execution time of the analytical queries, I work with a benchmarking dataset 
 and run 5 analytical queries to query the data from each store.
