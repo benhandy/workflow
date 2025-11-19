debafefcda894a7d7e316fdd42947f36c20c0ced
Benjamin Handy <benhandy9@gmail.com>
2025-11-19 11:16:41 -0500

    Update electronictrading.py

--- electronictrading.py
+++ electronictrading.py
@@ -6,7 +6,7 @@
 ## run through vs code, conda with jupyter notebook, API from databento or alpaca
 
 ## This approach is meant to test and emulate a singular niche trading strategy for fundamentally viable small-cap and mid-cap stocks experiencing sudden, retail-driven liquidity shocks (volatility dislocations) 
-## mitigating risk and delivering outsized moves in a systematic, rule-driven manner
+## while mitigating risk and delivering outsized moves in a systematic, rule-driven manner
 ## When the below mechanical trading approach is combined with a high-fidelity data pipeline, a sentiment analysis model, and extreme backtests (easier said than done)  it may or may not prove to have a positive expected value
 ## thus serving as a viable systematic trading signal
 ##As a resource to aid the development of the above trading strategy, I will continue to refine the model's predictive power,
